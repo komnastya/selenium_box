f66d7586ebf385c5d0a8744623a94b71f1511d92
Nastassia Kamiakova <nastassia294@gmail.com>
2021-12-03 16:48:58 +0300

    Clean up and improve code.

--- SELENIUM/se_03_3_select_option.py
+++ SELENIUM/se_03_3_select_option.py
@@ -9,25 +9,30 @@ actions = ActionChains(driver)
 
 driver.get("C:/Users/root/Projects/selenium_box/tests.html")
 
-element = driver.find_element_by_xpath("//*[@id='states']")
-all_options = element.find_elements_by_tag_name("option")
+form = driver.find_element_by_id("states")
+states_in_form = form.find_elements_by_tag_name("option")
 
-states = []
-count = 0
+# This function can be written in another module and be imported when we need it for testing:
+def count_options_in_form(options):
+    output = []
+    count = 0
+    for option in options:
+        time.sleep(1)
+        count += 1
+        value = option.get_attribute("value")
+        print(f"The {count}-th state in the list is: ", value)
+        output.append(value)
+        option.click()
+    return output, count
 
-# example from the tutorial:
-for option in all_options:
-    time.sleep(1)
-    count += 1
-    print(f"The {count}-th state in the list is: ", option.get_attribute("value"))
-    states.append(option.get_attribute("value"))
-    option.click()
+
+lst_of_states, quantity_of_states = count_options_in_form(states_in_form)
 
 driver.close()
 
-expected_result = ['Alabama', 'Alaska', 'Arizona', 'California', 'Colorado', 'Florida', 'Georgia', 'Hawaii', 'Kansas',
-                   'Michigan']
-result = states
 
 def test_list_of_states():
-    assert result == expected_result
+    assert lst_of_states == ['Alabama', 'Alaska', 'Arizona', 'California', 'Colorado', 'Florida', 'Georgia', 'Hawaii',
+                      'Kansas', 'Michigan']
+    assert len(lst_of_states) == quantity_of_states
+    assert len(lst_of_states) == 10
